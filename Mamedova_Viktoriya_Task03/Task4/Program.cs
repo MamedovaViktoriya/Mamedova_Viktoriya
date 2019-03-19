65884bea1d37e2266be4c7580ea330e5224ce394
Mamedova Viktoriya <48247857+MamedovaViktoriya@users.noreply.github.com>
2019-03-20 00:30:54 +0300

    Update Program.cs

--- Mamedova_Viktoriya_Task03/Task4/Program.cs
+++ Mamedova_Viktoriya_Task03/Task4/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +10,7 @@ namespace Task4
         static int SumArray(int[,] array)
         {
             int sum = 0;
+
             for (int i = 0; i < array.GetLength(0); i++)
             {
                 for (int j = 0; j < array.GetLength(1); j++)
@@ -20,34 +21,36 @@ namespace Task4
                     }
                 }
             }
+
             return sum;
         }
 
         static void Main(string[] args)
         {
             Console.WriteLine("Двумерный массив случайных чисел:");
-            int[,] RandomArray = new int[3, 3];
+            int[,] randomArray = new int[3, 3];
             Random random = new Random();
 
-            for (int i = 0; i < RandomArray.GetLength(0); i++)
+            for (int i = 0; i < randomArray.GetLength(0); i++)
             {
-                for (int j = 0; j < RandomArray.GetLength(1); j++)
+                for (int j = 0; j < randomArray.GetLength(1); j++)
                 {
-                    RandomArray[i, j] = random.Next(-15, 15);
-                }
-        }
-
-            for (int i = 0; i < RandomArray.GetLength(0); i++)
-            {
-                for (int j = 0; j < RandomArray.GetLength(1); j++)
-
-                {
-                    Console.WriteLine($"Элемент[{i + 1},{j + 1}] = {RandomArray[i, j]} ");
+                    randomArray[i, j] = random.Next(-15, 15);
                 }
             }
 
-            Console.WriteLine($"Сумма элементов массива, стоящих на четных позициях равна = {SumArray(RandomArray)}");
+            for (int i = 0; i < randomArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < randomArray.GetLength(1); j++)
+
+                {
+                    Console.WriteLine($"Элемент[{i + 1},{j + 1}] = {randomArray[i, j]} ");
+                }
+            }
+
+            Console.WriteLine($"Сумма элементов массива, стоящих на четных позициях равна = {SumArray(randomArray)}");
             Console.ReadKey();
         }
     }
 }
+
