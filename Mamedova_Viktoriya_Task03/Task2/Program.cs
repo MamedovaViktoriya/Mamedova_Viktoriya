b508788928192af5becfbe79e24564a86d1ca0bb
Mamedova Viktoriya <48247857+MamedovaViktoriya@users.noreply.github.com>
2019-03-20 00:28:08 +0300

    Update Program.cs

--- Mamedova_Viktoriya_Task03/Task2/Program.cs
+++ Mamedova_Viktoriya_Task03/Task2/Program.cs
@@ -50,7 +50,67 @@ namespace Task2
                         RandomArray = ChangeToZero(RandomArray);
                         Console.WriteLine($"Элемент измененного массива[{i + 1},{j + 1},{k + 1}] = {RandomArray[i, j, k]} ");
                     }
+                }using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static int[,,] ChangeToZero(int[,,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    for (int k = 0; k < array.GetLength(2); k++)
+                    {
+                        if (array[i, j, k] > 0)
+                        {
+                            array[i, j, k] = 0;
+                        }
+                    }
                 }
+            }
+
+            return array;
+        }
+
+        static void Main(string[] args)
+        {
+            int[,,] randomArray = new int[3, 3, 3];
+            Random random = new Random();
+
+            for (int i = 0; i < randomArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < randomArray.GetLength(1); j++)
+                {
+                    for (int k = 0; k < randomArray.GetLength(2); k++)
+                    {
+                        randomArray[i, j, k] = random.Next(-10, 10);
+                    }
+                }
+            }
+
+            for (int i = 0; i < randomArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < randomArray.GetLength(1); j++)
+                {
+                    for (int k = 0; k < randomArray.GetLength(2); k++)
+                    {
+                        Console.WriteLine($"Элемент исходного массива[{i + 1},{j + 1},{k + 1}] = {randomArray[i, j, k]} ");
+                        randomArray = ChangeToZero(randomArray);
+                        Console.WriteLine($"Элемент измененного массива[{i + 1},{j + 1},{k + 1}] = {randomArray[i, j, k]} ");
+                    }
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
             }
 
             Console.ReadKey();
