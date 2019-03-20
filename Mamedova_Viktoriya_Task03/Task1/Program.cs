b084b57be5f91db9cc5ec19dce9b7571dac423b9
Mamedova Viktoriya <48247857+MamedovaViktoriya@users.noreply.github.com>
2019-03-20 10:35:53 +0300

    Update Program.cs

--- Mamedova_Viktoriya_Task03/Task1/Program.cs
+++ Mamedova_Viktoriya_Task03/Task1/Program.cs
@@ -8,39 +8,13 @@ namespace Task1
 {
     class Program
     {
-        static int[] SortArray(int[] array)
-        {
-            int temp = 0;
-
-            for (int i = 0; i < array.Length - 1; i++)
-            {
-                for (int j = i + 1; j < array.Length; j++)
-                {
-                    if (array[i] > array[j])
-                    {
-                        temp = array[i];
-                        array[i] = array[j];
-                        array[j] = temp;
-                    }
-                }
-            }
-
-            return array;
-        }
-
         static void Main(string[] args)
         {
             Console.WriteLine("Одномерный массив случайных чисел:");
             int[] randomArray = new int[15];
             randomArray = HelperFunction.CreateRandomArray(randomArray);
-            randomArray = SortArray(randomArray);
-
-            for (int i = 0; i < randomArray.Length; i++)
-
-            {
-                Console.WriteLine($"Элемент[{i}] = {randomArray[i]} ");
-            }
-
+            randomArray = HelperFunction.SortArray(randomArray);
+            randomArray = HelperFunction.ShowArray(randomArray);
             Console.WriteLine($"Минимальное значение массива = {randomArray[0]}");
             Console.WriteLine($"Максимальное значение массива = {randomArray[randomArray.Length - 1]}");
             Console.ReadKey();
