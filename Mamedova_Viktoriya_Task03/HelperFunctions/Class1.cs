71892ff14da4b2a49861c92f82c2be2ac056bfe3
Mamedova Viktoriya <48247857+MamedovaViktoriya@users.noreply.github.com>
2019-03-20 10:32:35 +0300

    Update Class1.cs

--- Mamedova_Viktoriya_Task03/HelperFunctions/Class1.cs
+++ Mamedova_Viktoriya_Task03/HelperFunctions/Class1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,8 +15,39 @@ namespace HelperFunctions
             {
                 randomArray[i] = random.Next(-15, 15);
             }
-
             return randomArray;
         }
-    }
+
+        public static int[] SortArray(int[] array)
+        {
+            int temp = 0;
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+
+            return array;
+        }
+
+        public static int[] ShowArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"Элемент[{i}] = {array[i]} ");
+            }
+
+            return array;
+        }
+    }      
 }
+
+
